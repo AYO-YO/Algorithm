22a1b03d7a4a54f55b433ff36fc909bd854e20db
ayo_yo <gxlandzcx@gmail.com>
2022-03-19 21:06:53 +0800

    T1236

---
+++ src/cn/ayo/jsk/T1236.java
@@ -0,0 +1,12 @@
+package cn.ayo.jsk;
+
+import java.util.Scanner;
+
+//https://nanti.jisuanke.com/t/T1236
+public class T1236 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        System.out.println(((m + 1) * m) / 2);
+    }
+}
