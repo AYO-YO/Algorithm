2712ac38d9b6bf1183baacdf87f0f8e693a63ed7
ayo_yo <gxlandzcx@gmail.com>
2022-04-12 20:53:04 +0800

    一个更优秀的解法

--- LQB/G.py
+++ LQB/G.py
@@ -1,4 +1,6 @@
 import functools
+import math
+import time
 
 
 @functools.lru_cache(10 ** 6)
@@ -9,11 +11,31 @@ def jc(n):
 
 
 n = int(input())
-v = [0, 1]
-for i in range(1, n + 1):
-    m = i - 1
-    jcc = jc(m)
-    l = (m * i) // 2
-    v[1] = ((v[0] * i + l * jcc) % 998244353)
-    v[0] = v[1]
-print(v[1])
+
+
+def run1():
+    start = time.time()
+    v = [0, 1]
+    for i in range(1, n + 1):
+        m = i - 1
+        jcc = jc(m)
+        l = (m * i) // 2
+        v[1] = ((v[0] * i + l * jcc) % 998244353)
+        v[0] = v[1]
+    print(v[1])
+    end = time.time()
+    print(end - start)
+
+
+# run1()
+# print()
+
+
+def run2():
+    start = time.time()
+    print((sum(range(n)) * math.factorial(n) // 2) % 998244353)
+    end = time.time()
+    print('run2:', end - start)
+
+
+run2()
