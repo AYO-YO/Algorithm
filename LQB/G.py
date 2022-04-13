2602d1ecad9b81f480546b94fbe872993e376b7c
ayo_yo <gxlandzcx@gmail.com>
2022-04-13 17:38:55 +0800

    一个更优秀的解法（run3）

--- LQB/G.py
+++ LQB/G.py
@@ -24,10 +24,12 @@ def run1():
         v[0] = v[1]
     print(v[1])
     end = time.time()
-    print(end - start)
+    print('run1:', end - start)
 
 
 # run1()
+
+
 # print()
 
 
@@ -38,4 +40,40 @@ def run2():
     print('run2:', end - start)
 
 
-run2()
+# run2()
+
+
+def run3():
+    start = time.time()
+    m = 998244353
+    fact = 1
+    ans = [0]
+    for i in range(2, n + 3):
+        fact *= i - 1
+        fact %= m
+        ans.append((ans[-1] * i + fact * (i - 1) * i // 2) % m)
+    print(ans[n - 1])
+    end = time.time()
+    print('run3:', end - start)
+
+
+run3()
+
+
+def run4():
+    start = time.time()
+    v = [0, 1]
+    fact = 1
+    for i in range(2, n + 1):
+        fact *= i - 1
+        fact %= 998244353
+        m = i - 1
+        l = (m * i) // 2
+        v[1] = ((v[0] * i + l * fact) % 998244353)
+        v[0] = v[1]
+    print(v[1])
+    end = time.time()
+    print('run4:', end - start)
+
+
+run4()
