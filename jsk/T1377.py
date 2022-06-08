aa0a98cd559521341a0bf075408b5602a39f8434
ayo_yo <gxlandzcx@gmail.com>
2022-06-08 22:01:26 +0800

    T1377 - 易

--- jsk/T1377.py
+++ jsk/T1377.py
@@ -2,8 +2,7 @@
 
 n = int(input())
 
-ans = []
-ans.append(' ' * (n - 1) + '*')
+ans = [' ' * (n - 1) + '*']
 for i in range(2, n + 1):
     ans.append(' ' * (n - i) + '*' + (2 * (i - 1) - 1) * ' ' + '*')
 
