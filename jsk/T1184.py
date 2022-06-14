686ff4deaeda363b2b945568cf343c5564696f81
ayo_yo <gxlandzcx@gmail.com>
2022-06-14 20:15:26 +0800

    T1184 - 易

--- jsk/T1184.py
+++ jsk/T1184.py
@@ -1,2 +1,24 @@
 """https://nanti.jisuanke.com/t/T1184"""
 
+
+def get_y(x):
+    return 2 * x + 1, 3 * x + 1
+
+
+k, x = map(int, input().split(','))
+
+
+def dfs(k):
+    global x
+    if k == x:
+        print('YES')
+        exit()
+    if k > x:
+        return
+    datas = get_y(k)
+    for i in datas:
+        dfs(i)
+
+
+dfs(k)
+print('NO')
