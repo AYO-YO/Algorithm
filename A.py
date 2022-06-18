81377538cd1faf805e59c39f162ba7028f5f5931
ayo_yo <gxlandzcx@gmail.com>
2022-06-18 15:24:22 +0800

    13届蓝桥杯国赛PB-A

---
+++ A.py
@@ -0,0 +1,26 @@
+import functools
+
+import numpy as np
+
+
+@functools.lru_cache()
+def fib(n):
+    if n <= 2:
+        return 1
+    return np.mod(np.add(fib(n - 1), fib(n - 2)), 10)
+
+
+def calc(n):
+    s = str(n)
+    if s[0] == '1':
+        return int('1' + (len(s) - 3) * '3' + '4')
+    else:
+        return int('2' + (len(s) - 3) * '6' + '7')
+
+
+m = str(202202011200)
+ans = 0
+for i in range(len(m)):
+    t = int(m[i:])
+    ans += calc(t)
+print(ans)
